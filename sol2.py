648be163b7944c11b767090854bdab2feb6369d3
ankit <ankit.chachada@gmail.com>
2016-11-09 03:01:24 -0800

    solution 2

---
+++ sol2.py
@@ -0,0 +1,12 @@
+def sol2(n):
+    a,b = 1,1
+    c = 0
+    for i in range(n):
+        a,b = b,a+b
+        if(a%2 == 0):
+            c += a
+        if (c >= 4000000):
+            break
+    return c
+    
+print(sol2(40))
